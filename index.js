c28b01fe93aa18511a9e66535ce5bfc6ec9c7c54
sahil <sahilagarwal3005@gmail.com>
2024-08-25 12:11:09 +0530

    changes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 // Middleware for handling errors
 app.use((err, req, res, next) => {
     console.error(err.stack);
